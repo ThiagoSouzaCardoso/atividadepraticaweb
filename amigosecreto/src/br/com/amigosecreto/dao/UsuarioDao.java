c290197954654ebf7114eeb34eb3591bc5049017
RM48120 <RM48120@L2901MICRO25.fiap.com.br>
2016-06-13 20:07:05 -0300

    consulta amigo secreto

--- amigosecreto/src/br/com/amigosecreto/dao/UsuarioDao.java
+++ amigosecreto/src/br/com/amigosecreto/dao/UsuarioDao.java
@@ -1,7 +1,12 @@
 package br.com.amigosecreto.dao;
 
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import javax.persistence.Query;
+
+import br.com.amigosecreto.dao.util.JpaUtil;
 import br.com.amigosecreto.entity.Usuario;
 
 public class UsuarioDao extends GenericDao<Usuario> implements Dao<Usuario>{
@@ -11,8 +16,15 @@ public class UsuarioDao extends GenericDao<Usuario> implements Dao<Usuario>{
 	} 
 
 	public Set<Usuario> findAllFriendsSecret(Usuario usuario){
-		
-		return null;
+		em = JpaUtil.getEntityManager();
+		em.getTransaction().begin();
+		String jpql = "from Usuario user where user.amigosSorteado is null and user.cpf <> :pCPF";
+		Query query = em.createQuery(jpql);
+		query.setParameter("pCPF", usuario.getCpf());
+		List<Usuario> resultList = query.getResultList();
+		em.getTransaction().commit();
+		em.close();
+		return resultList.stream().collect(Collectors.toSet());
 	}
 	
 }
--- amigosecreto/src/br/com/amigosecreto/entity/Usuario.java
+++ amigosecreto/src/br/com/amigosecreto/entity/Usuario.java
@@ -1,20 +1,42 @@
 package br.com.amigosecreto.entity;
 
+import static javax.persistence.TemporalType.DATE;
+
 import java.io.Serializable;
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
 
 @Entity
+@Table(name="usuario")
 public class Usuario implements Serializable {
 
 	private static final long serialVersionUID = 2927146768339266249L;
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="id_usuario")
+	private Integer id;
+	
+	@Column(name="nome")
 	private String nome;
+	
+	@Column(name="cpf")
 	private String cpf;
-	private Date data;
+	
+	@Temporal(DATE)
+	private Date dataNascimento;
+	
+	@OneToOne
 	private Usuario amigosSorteado;
-
+	
 	public String getNome() {
 		return nome;
 	}
@@ -31,20 +53,53 @@ public class Usuario implements Serializable {
 		this.cpf = cpf;
 	}
 
-	public Date getData() {
-		return data;
-	}
-
-	public void setData(Date data) {
-		this.data = data;
-	}
-
 	public Usuario getAmigosSorteado() {
 		return amigosSorteado;
 	}
 
 	public void setAmigosSorteado(Usuario amigosSorteado) {
 		this.amigosSorteado = amigosSorteado;
+	}
+
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		return true;
 	}
 
 }
